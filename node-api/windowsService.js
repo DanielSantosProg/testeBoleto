d23cfe50374bba0eed6e92790c21723793851909
DanielSantosProg <danielsantos_lol@hotmail.com>
2025-07-16 10:38:39 -0300

    Minor fix to the windowsService directory

--- node-api/windowsService.js
+++ node-api/windowsService.js
@@ -3,8 +3,7 @@ const Service = require("node-windows").Service;
 const svc = new Service({
   name: "API Boletos Bradesco",
   description: "API como serviço para Boletos Bradesco",
-  script:
-    "C:\\Users\\Meu computador\\Documents\\GitHub\\Testes\\TesteBoleto\\node-api",
+  script: __dirname,
 });
 svc.on("install", () => {
   console.log("Serviço instalado!");
